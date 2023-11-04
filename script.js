8a1fa567ef2fd5381a0c8ba27ee65e4d6f11fef0
Mathew Sebastian E <mathew.e@experionglobal.local>
2023-11-04 12:23:18 +0530

    feat: added ui changes when HOLD button is pressed

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-'use strict';
+function changeColor(flag) {
+  if (flag == 0) {
+    var p1 = document.querySelector(`.player--0`);
+    p1.classList.remove('player--active');
+    var p2 = document.querySelector(`.player--1`);
+    p2.classList.add('player--active');
+  } else {
+    var p2 = document.querySelector(`.player--0`);
+    p2.classList.remove('player--active');
+    var p1 = document.querySelector(`.player--1`);
+    p1.classList.add('player--active');
+  }
+}
