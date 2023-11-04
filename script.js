aeaa1694e4f96d89c5a3980c0609e9d5e810a1fd
Aljo Biju <aljo.biju@experionglobal.local>
2023-11-04 12:36:01 +0530

    fix:-minor changes

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// let current0= document.getElementById("current--0");
-// let current1= document.getElementById("current--1");
-
-// let score1 = 49;
-// document.getElementById('score--0').textContent = `${score1}`;
-
-// let score2 = 89;
-// document.getElementById('score--1').textContent = `${score2}`;
-
-// let flag=0;
-// if (flag==0){
-//     score1=score1+
-// }
-// var score0 = 8;
-// var current0 = 9;
-// var flag = 0;
+var flag = 0;
 function btnHold() {
   if (flag == 0) {
     var current0 = document.getElementById('current--0');
