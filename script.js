34c8eeb3ca797fcd11963c1f801a0db727011f4a
Madhav B <madhav.b@experionglobal.local>
2023-11-04 15:35:46 +0530

    fix: Make variables Global

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 // Ashna Randomizer
 
-document.addEventListener('DOMContentLoaded', function () {
-  let scores, currentScore, activePlayer, isPlaying;
+var scores, currentScore, activePlayer, isPlaying, dice;
 
-  const diceElement = document.querySelector('.dice');
-  const rollButton = document.querySelector('.btn--roll');
+const diceElement = document.querySelector('.dice');
+const rollButton = document.querySelector('.btn--roll');
 
-  function init() {
-    scores = [0, 0];
-    currentScore = 0;
-    activePlayer = 0;
-    isPlaying = true;
+document.getElementById('current--0').textContent = '0';
+document.getElementById('current--1').textContent = '0';
 
-    document.getElementById('current--0').textContent = '0';
-    document.getElementById('current--1').textContent = '0';
-  }
+scores = [0, 0];
+currentScore = 0;
+activePlayer = 0;
+isPlaying = true;
 
-  init();
-  function rollDice() {
-    if (isPlaying) {
-      const dice = Math.floor(Math.random() * 6) + 1;
-      diceElement.style.display = 'block';
-      diceElement.src = 'dice-' + dice + '.png';
+function rollDice() {
+  if (isPlaying) {
+    dice = Math.floor(Math.random() * 6) + 1;
 
-      if (dice !== 1) {
-        currentScore += dice;
-        document.getElementById('current--' + activePlayer).textContent =
-          currentScore;
-      }
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById('current--' + activePlayer).textContent =
+        currentScore;
     }
   }
-  rollButton.addEventListener('click', rollDice);
-});
+}
+rollButton.addEventListener('click', rollDice);
 
 // Navya Dice Image
 
